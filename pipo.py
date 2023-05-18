f81758510f7f8830487c1cd5db5bf6e7d968202f
unknown <demonmuchnoy@gmail.com>
2023-05-18 18:19:35 +0500

    ping pong

--- pipo.py
+++ pipo.py
@@ -1,4 +1,3 @@
-
 from pygame import *
 '''Необходимые классы'''
 
@@ -34,21 +33,20 @@ class Player(GameSprite):
 
 
 #игровая сцена:
-back = (200, 255, 255) #цвет фона (background)
+back = (255, 255, 255) 
+
+#цвет фона (background)
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
-
-
-#флаги, отвечающие за состояние игры
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
 
 
-#создания мяча и ракетки   
+#мяча+ракетка   
 racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
@@ -81,19 +79,13 @@ while game:
            speed_x *= -1
            speed_y *= 1
       
-       #если мяч достигает границ экрана, меняем направление его движения
+       #если мяч ...
        if ball.rect.y > win_height-50 or ball.rect.y < 0:
            speed_y *= -1
-
-
-       #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
        if ball.rect.x < 0:
            finish = True
            window.blit(lose1, (200, 200))
            game_over = True
-
-
-       #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
        if ball.rect.x > win_width:
            finish = True
            window.blit(lose2, (200, 200))
